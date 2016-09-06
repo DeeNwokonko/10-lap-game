72ac17fc14518a70449572734a4a82bc14197340
Ikedieze <iyksonic2006@yahoo.com>
2016-09-06 13:59:01 +0100

    third commit

---
+++ Chidinma/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Chidinma
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+
+           Stopwatch k = new Stopwatch();
+
+            for (int i=1; i<=10; i++)
+            {
+                k.Start();
+                 for (int j=1; j<=400; j++)
+                {
+                    Console.WriteLine(j + "miles");
+                    //get the stop watch to start at j=0 and stop at j=399 
+                }
+                k.Stop();
+
+                Console.WriteLine("lap "+ i +" "+ k.ElapsedMilliseconds + " milliseconds"  );
+                Console.ReadLine();
+
+            }
+
+        }
+
+    }
+}
